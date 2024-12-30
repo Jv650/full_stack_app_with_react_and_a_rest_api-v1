eb4879df5d87d9c5e782b7d3070982589199d256
Jv650 <141464576+Jv650@users.noreply.github.com>
2024-12-30 11:42:55 -0800

    edits

--- client/src/components/CourseDetail.jsx
+++ client/src/components/CourseDetail.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import Markdown from "react-markdown";
+import { UserContext } from "../context/UserContext";
 import { useParams, useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom"; //import Link from react router
 import { api } from "../utils/apiHelper";
@@ -9,6 +10,7 @@ const CourseDetail = () => {
   const [course, setCourse] = useState([{}]); //empty object bc its expecting props such as title, description, etc.
   const { id } = useParams(); //get course id from the api
   const navigate = useNavigate();
+  const { authUser } = useContext(UserContext);
 
   //fetch /api/courses/:id from API
   useEffect(() => {
@@ -74,49 +76,59 @@ const CourseDetail = () => {
   // }
 
   return (
-    <div className="actions--bar">
-      <div className="wrap">
-        <Link to={`/${id}/update`}>
-          <a className="button">Update Course</a>
-        </Link>
-        <a
-          className="button"
-          onClick={handleDelete}
-          /*EDIT onClick={"DELETE"}*/
-        >
-          Delete Course
-        </a>
-        <Link to="/">
-          <a className="button button-secondary">Return to List</a>
-        </Link>
+    <main>
+      <div className="actions--bar">
+        <div className="wrap">
+          {authUser && authUser.id === course.id && (
+            <>
+              <Link className="button" to={`/courses/${id}/update`}>
+                Update Course
+              </Link>
+              <button
+                className="button"
+                onClick={handleDelete}
+                /*EDIT onClick={"DELETE"}*/
+              >
+                Delete Course
+              </button>
+            </>
+          )}
+          <Link to="/">
+            <a className="button button-secondary">Return to List</a>
+          </Link>
+        </div>
       </div>
 
       <div className="wrap">
-        <h2>{course.title}</h2>
-        <form>
-          <div className="main--flex">
-            <div>
-              <h3 className="course--course--title">Course</h3>
-              <h4 className="course--name">
-                <Markdown>{course.description}</Markdown>
-              </h4>
-              <p>By {course.authorName}</p>
-            </div>
-            <div>
-              <h3 className="course--course--title">Estimated Time</h3>
-              <p>
-                <Markdown>{course.estimatedTime}</Markdown>
-              </p>
-
-              <h3 className="course--course--title">Materials Needed</h3>
-              <ul className="course--course--list">
-                <Markdown>{course.materialsNeeded}</Markdown>
-              </ul>
-            </div>
+        <h2>Course Detail</h2>
+        <div className="main--flex">
+          <div>
+            <h3 className="course--detail--title">Course</h3>
+            <h4 className="course--name">
+              <Markdown>{course.title}</Markdown>
+            </h4>
+            <p>
+              By {""}
+              {course.user
+                ? `${course.user.firstName} ${course.user.lastName}`
+                : ""}
+            </p>
+            <Markdown>{course.description}</Markdown>
           </div>
-        </form>
+          <div>
+            <h3 className="course--detail--title">Estimated Time</h3>
+            <p>
+              <Markdown>{course.estimatedTime}</Markdown>
+            </p>
+
+            <h3 className="course--course--title">Materials Needed</h3>
+            <ul className="course--course--list">
+              <Markdown>{course.materialsNeeded}</Markdown>
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
+    </main>
   );
 };
 
