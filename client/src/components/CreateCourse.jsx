9d4a62cd7f3b8ec94553338b9b402f28f9e7ec58
Jv650 <141464576+Jv650@users.noreply.github.com>
2025-01-02 12:19:08 -0800

    final edit line 5

--- client/src/components/CreateCourse.jsx
+++ client/src/components/CreateCourse.jsx
@@ -2,7 +2,6 @@ import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 import { api } from "../utils/apiHelper";
-import ErrorsDisplay from "./Error";
 
 const CreateCourse = () => {
   const navigate = useNavigate();
